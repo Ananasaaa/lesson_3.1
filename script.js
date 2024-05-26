5bf3d6bde94b00c95cffce99f1ad9be284243a72
Ananasaaa <kairyakn@gmail.com>
2024-05-26 23:37:36 +0300

    lesson_3.1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Homework 3.1
+
+console.log(
+    typeof 1, 
+    typeof "string", 
+    typeof true,
+    typeof undefined,
+    typeof null,
+    typeof Symbol(),
+    typeof {},
+    typeof [],
+    typeof function () {}
+);
